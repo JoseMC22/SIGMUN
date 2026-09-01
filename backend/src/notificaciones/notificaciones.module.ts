24de851da8f67e7d929cb80432a32cf7d7a41d55
manuelvaezc <manuelvaezc@gmail.com>
2026-09-01 10:47:12 -0500

    feat(notificaciones): add reporte de constancia de exigibilidad with search and export

--- backend/src/notificaciones/notificaciones.module.ts
+++ backend/src/notificaciones/notificaciones.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+﻿import { Module } from '@nestjs/common';
 import { MantenimientoNotificadoresModule } from './mantenimiento-notificadores/mantenimiento-notificadores.module';
 import { ReporteCargosModule } from './reporte-cargos/reporte-cargos.module';
+import { ReporteConstanciaExigibilidadModule } from './reporte-constancia-exigibilidad/reporte-constancia-exigibilidad.module';
 
 @Module({
-  imports: [MantenimientoNotificadoresModule, ReporteCargosModule],
+  imports: [
+    MantenimientoNotificadoresModule,
+    ReporteCargosModule,
+    ReporteConstanciaExigibilidadModule,
+  ],
 })
 export class NotificacionesModule {}
